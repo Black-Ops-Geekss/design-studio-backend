7473588d6d49cccb39fe87cc2218211a668c4959
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-08-14 20:38:39 +0300

    created new schema for orders, added new end point for add orders

---
+++ modules/unsplash/getSelection.js
@@ -0,0 +1,13 @@
+const SaveDesigns = require('./schema');
+
+function getSelection (req, res) {
+    const id = req.params.id;
+    SaveDesigns.find({_id:id}, (error, data) => {
+        if (error) console.log(`error reading from the db: ${error}`);
+        else res.send(data);
+    }).catch(err => {
+        console.log(err);
+    }
+    );
+}
+module.exports = getSelection;
